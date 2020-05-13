4ff042e33fbf7d8b9c8af21ca4a2943a4f59aba6
saidtazi <said.altazi@gmail.com>
2020-05-13 05:22:59 +0000

    add existing files

---
+++ assoc/assoc/doctype/association/association.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, TAZI Said and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Association', {
+	// refresh: function(frm) {
+
+	// }
+});
